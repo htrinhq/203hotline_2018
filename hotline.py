abfb31db4c5b38cfb1b919136aafba94200b267a
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-03-18 10:45:15 +0100

    check error before printing anything

--- hotline.py
+++ hotline.py
@@ -10,9 +10,7 @@ from poisson import poisson_law
 
 def hotline_two(data: [int]):
     """Hotline function with 2 args."""
-    try:
-        data[1] > data[2]
-    except:
+    if data[0] < data[1]:
         error("n must be superior to k.\n")
     print('{:g}-combination of a {:g} set:' .format(data[1], data[0]))
     print('{:d}' .format(binomial_coefficient(data[0], data[1])))
